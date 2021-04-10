7d929f5a79b2ece43cef0375192959ecbca7cb79
Amro dandashli <amrdandashli@gmail.com>
2021-04-10 18:57:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const append = ["Milo", "Otis", "Garfield"] ;
 const removeFirst = ["Milo", "Otis", "Garfield"] ;
 const removeLast = ["Milo", "Otis", "Garfield"] ;
-
+const prepend = ["Milo", "Otis", "Garfield"] ;
   append.push("Odie") ;
-    prepend.shift();
+    prepend.unshift("Odie");
     removeLast.pop(-1);
-     removeFirst.unshift("Odie");
+     removeFirst.shift();
